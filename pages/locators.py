4d71f2e5638b64df41c81c473557b580504cca20
DESKTOP-UNOF096\Meoz <codomeoz@gmail.com>
2020-12-29 22:36:23 +0200

    Test case for Product page

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,13 @@ class LoginPageLocators:
     LOGIN_URL = "login"
     LOGIN_FORM = (By.ID, "login_form")
     REGISTER_FORM = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    URL_WITH_PROMO = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    URL = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+    SUBMIT_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div>div")
+    SUCCESS_MESSAGE_PRICE = (By.CSS_SELECTOR, "#messages div>div p")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
---
+++ pages/product_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    product_price = ""
+    product_name = ""
+
+    def add_to_basket(self):
+        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        self.browser.find_element(*ProductPageLocators.SUBMIT_BUTTON).click()
+        # print("------------------------ DEBUG INFO FOR add_to_basket function------------------------")
+        # print("product_name variable value = " + self.product_name)
+        # print("product_price variable value = " + self.product_price)
+
+    def check_that_product_added_to_basket(self):
+        # print("------------------------ DEBUG INFO FOR check_that_product_added_to_basket function------------------------")
+        # print("Success message value " + self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGE)[0].text)
+        assert self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGE)[0].text == self.product_name + " has been added to your basket."
+
+    def check_that_basket_value_changed(self):
+        # print("------------------------ DEBUG INFO FOR check_that_basket_value_changed function------------------------")
+        # print(self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGE_PRICE)[0].text)
+        assert self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGE_PRICE)[0].text == "Your basket total is now " + self.product_price
